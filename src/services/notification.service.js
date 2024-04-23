9cf64a9ceb381940cde72338849f7d3092b57bbe
iftekhar841 <mdiftekhar841@gmail.com>
2024-04-23 12:13:43 +0530

    Modified the code

--- src/services/notification.service.js
+++ src/services/notification.service.js
@@ -1,39 +1,46 @@
 import { isValidObjectId } from "mongoose";
 import Notification from "../models/notification.model.js";
 
-const getNotifications = async (queryParams, loggedInUser) => {
-  const { type } = queryParams;
-  console.log("type", type);
-  const condition = {
-    type: {
-      $options: "i",
-      $regex: type,
-    },
-    isRead: false,
-    user: loggedInUser,
+  const getNotifications = async (queryParams, loggedInUser) => {
+    const { type } = queryParams;
+    console.log("type", type);
+    const condition = {
+      type: {
+        $options: "i",
+        $regex: type,
+      },
+      // isRead: false,
+      user: loggedInUser,
+    };
+    console.log("condition", condition);
+
+    // Query to find notifications
+    const notificationsQuery = Notification.find(condition).sort({
+      createdAt: -1,
+    });
+    const countCondition = {
+      type: {
+        $options: "i",
+        $regex: type,
+      },
+      isRead: false,
+      user: loggedInUser,
+    };
+    // Query to count the total number of notifications
+    const countQuery = Notification.countDocuments(countCondition);
+
+    // Execute both queries in parallel
+    const [notifications, countNotification] = await Promise.all([
+      notificationsQuery,
+      countQuery,
+    ]);
+
+    console.log("Notifications", notifications);
+    console.log("Total count of notifications", countNotification);
+
+    // Return both notifications and count
+    return { notifications, countNotification };
   };
-  console.log("condition", condition);
-
-  // Query to find notifications
-  const notificationsQuery = Notification.find(condition).sort({
-    createdAt: -1,
-  });
-
-  // Query to count the total number of notifications
-  const countQuery = Notification.countDocuments(condition);
-
-  // Execute both queries in parallel
-  const [notifications, countNotification] = await Promise.all([
-    notificationsQuery,
-    countQuery,
-  ]);
-
-  console.log("Notifications", notifications);
-  console.log("Total count of notifications", countNotification);
-
-  // Return both notifications and count
-  return { notifications, countNotification };
-};
 
 const updateNotification = async (paramsData) => {
   const { notificationId } = paramsData;
@@ -58,16 +65,16 @@ const readNotification = async (loggedInUser) => {
     throw new ApiError(400, "Invalid loggedInUser Format");
   }
   console.log("loggedInUser", loggedInUser);
-  const notification = await Notification.updateMany(
+  const result  = await Notification.updateMany(
     { user: loggedInUser, isRead: false, type:"subscribe" },
-    { isRead: true }
+    { $set: { isRead: true },}
   );
-  console.log("notification", notification);
+  console.log("Update result:", result);
+  // Return the number of modified documents
 
-  const fetchNotificationsAfterRead = await Notification.find({ user: loggedInUser, isRead: true})
-  console.log("fetchNotificationsAfterRead", fetchNotificationsAfterRead);
- 
-  return fetchNotificationsAfterRead;
+  if((result.acknowledged === true) && (result.modifiedCount)) {
+      return result.modifiedCount;
+  }
 };
 
 export default {
